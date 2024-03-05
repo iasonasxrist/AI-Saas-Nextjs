11d8cc0bbfcf89a0774e1ffb017276a27b23ee32
iasonasxrist <iasonasxrist@gmail.com>
2024-03-05 18:21:28 +0200

    new

--- influenceai/src/trpc/index.ts
+++ influenceai/src/trpc/index.ts
@@ -2,17 +2,7 @@ import { TRPCError } from "@trpc/server/unstable-core-do-not-import";
 import { publicProcedure, router } from "./trpc";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
 
-export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
-    const { getUser } = getKindeServerSession();
-    const user = getUser();
-
-    // TODO :Check bearer token also
-    if (!user.id || !user.email) {
-      throw new TRPCError({ code: "UNAUTHORIZED" });
-    }
-    return { success: true };
-  }),
+expo
 });
 
 export type AppRouter = typeof appRouter;
